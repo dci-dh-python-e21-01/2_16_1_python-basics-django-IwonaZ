c542b7ddf6a16eaced975ac78fb8c163d694e316
IwonaZ <iwona.zdzieblo@gmail.com>
2022-01-12 11:15:30 +0100

    login and logout

--- course/common/views.py
+++ course/common/views.py
@@ -17,8 +17,10 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}!")
-            return redirect("home")
+            messages.success(
+                request, f"Account created for {username}! You are now able to login."
+            )
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "common/register.html", {"form": form})
